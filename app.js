78204cb4c917a507d1624df1f51c77d0b1c60d88
vd18 <vandandave18@gmail.com>
2021-03-24 18:23:02 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 
 var btntranslate = document.querySelector("#btn-translate");
 var texarea = document.querySelector("#tex-area");
+var outputbox = document.querySelector(".outputbox")
 
 
 
 
-btntranslate.addEventListener("click", function clickeventhandler(){
-    console.log("clicked!");
-    console.log("input", texarea.value)
+btntranslate.addEventListener("click", function clickeventhandler()
+{
+   outputbox.innerText =  texarea.value;
 }
 )
-
-
-
-
 
 
 
