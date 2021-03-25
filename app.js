530bb3c7a02e73cd53c7fd6f1cdf5ab9a2c5c919
vd18 <vandandave18@gmail.com>
2021-03-25 17:28:58 +0530

    final change

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 
 var btntranslate = document.querySelector("#btn-translate");
 var texarea = document.querySelector("#tex-area");
-var outputbox = document.querySelector(".outputbox")
+var outputbox = document.querySelector("#outputbox")
 
-var serverurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverurl = "https://api.funtranslations.com/translate/minion.json"
 
 function translateurl(text){
    return serverurl + "?" + "text=" + text
 }
 
 
+function handler(error){
+   console.log("errror occured " + error )
+   alert("something went wrong try again after some time")
 
 
-
-
+}
 function clickeventhandler(){
-// outputbox.innerText =  texarea.value;
+
    var inputText = texarea.value
 
    fetch(translateurl(inputText))
    .then(response => response.json())
-   .then(json => console.log(json.contents.translated))
-   
+   .then(json => {
+      var translatedtext = json.contents.translated
+      outputbox.innerText = translatedtext
+      
+   })
+   .catch(handler)
    };
-
-
-
 
 
 btntranslate.addEventListener("click", clickeventhandler)
