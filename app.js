ccd5caa36b76b7a241f23dec3ee79d48fe4f3479
vd18 <vandandave18@gmail.com>
2021-03-25 13:14:10 +0530

    server api url

--- app.js
+++ app.js
@@ -3,17 +3,32 @@ var btntranslate = document.querySelector("#btn-translate");
 var texarea = document.querySelector("#tex-area");
 var outputbox = document.querySelector(".outputbox")
 
+var serverurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-
-
-btntranslate.addEventListener("click", function clickeventhandler()
-{
-   outputbox.innerText =  texarea.value;
+function translateurl(text){
+   return serverurl + "?" + "text=" + text
 }
-)
 
 
 
+
+
+
+function clickeventhandler(){
+// outputbox.innerText =  texarea.value;
+   var inputText = texarea.value
+
+   fetch(translateurl(inputText))
+   .then(response => response.json())
+   .then(json => console.log(json.contents.translated))
+   
+   };
+
+
+
+
+
+btntranslate.addEventListener("click", clickeventhandler)
 
 
 
